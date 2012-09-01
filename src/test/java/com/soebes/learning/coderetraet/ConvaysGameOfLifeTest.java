9a957751b6dc9a3e5c0aa72908154f7dd460546d
Karl Heinz Marbaise <info@soebes.de>
2012-09-01 22:13:06 +0200

    - Added environment etc.

---
+++ src/test/java/com/soebes/learning/coderetraet/ConvaysGameOfLifeTest.java
@@ -0,0 +1,15 @@
+package com.soebes.learning.coderetraet;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+import org.testng.annotations.Test;
+
+public class ConvaysGameOfLifeTest {
+
+	@Test
+	public void firstTest() {
+		assertThat(true).isTrue();
+		
+	}
+
+}
